dff6067327e12849120d406f0e9f2f2499bb27ae
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-06-24 01:06:26 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,17 +2,22 @@ import { useAuth } from "hooks/useAuth";
 import { Button, Container, Text } from "./UserMenu.styled";
 import { useDispatch } from 'react-redux';
 import { logOut } from "redux/auth/authOperations";
-
-
+import { useNavigate } from 'react-router-dom';
 
 export default function UserMenu() {
   const dispatch = useDispatch();
   const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logOut());
+    navigate('/');
+  };
 
   return (
     <Container >
       <Text >Welcome, {user.name}</Text>
-      <Button type="button" onClick={() => dispatch(logOut())}>
+      <Button type="button" onClick={handleLogout}>
         Logout
       </Button>
     </Container>
